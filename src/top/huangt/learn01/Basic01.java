f1247633cdda4693887d5699ce79945772b8456a
LittleTry <1109394634@qq.com>
2018-06-27 01:38:17 +0800

    初始化

---
+++ src/top/huangt/learn01/Basic01.java
@@ -0,0 +1,12 @@
+package top.huangt.learn01;
+
+/**
+ * @Author: LittleTry
+ * @Date: 2018/6/27 1:28
+ */
+public class Basic01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
